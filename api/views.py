ccc3df17756d32a3cfaa8f931dfdf286441f31d5
Christophe Rimann <cjr2185@columbia.edu>
2018-10-19 00:42:55 -0400

    scoring?

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from .models import Person, Contract, Verifier_Contract
 from rest_framework import viewsets
 from .serializers import PersonSerializer, ContractSerializer, Verifier_ContractSerializer
 from django.http import HttpResponse
+import heapq
 # Create your views here.
 
 class PersonViewSet(viewsets.ModelViewSet):
@@ -19,7 +20,29 @@ class VerifierContractViewSet(viewsets.ModelViewSet):
 
 
 def score(request):
-	resulting_scores = Verifier_Contract.objects.all()
-	for x in resulting_scores:
-		print(x)
-	return render(request, "score.html", {})
+	contracts = Contract.objects.all()
+	fin = {}
+	for contract in contracts:
+		trues = 0
+		falses = 0
+		print(contract)
+		for verifier in contract.verify.all():
+			if verifier.verified: trues += 1
+			else: falses += 1
+		go = trues > falses
+		for verify in contract.verify.all():
+			if verify.verified == go:
+				if verify.verifier in fin: fin[verify.verifier] += 1
+	heap = []
+	print(fin)
+	heapq.heapify(heap)
+	for key, value in fin.items():
+		heapq.push(heap, (-1 * value, key))
+	print(heap)
+	fin = []
+	while heap:
+		temp = heapq.pop(fin)
+		fin.append((temp[1], -1 * temp[0]))
+	print(fin)
+	c = {'l':fin}
+	return render(request, "score.html", c)
