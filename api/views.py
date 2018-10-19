53297342971f8a1ecb282ab6338b2c2fce3e9c5e
Christophe Rimann <cjr2185@columbia.edu>
2018-10-19 00:22:53 -0400

    sum r3al sh1t h3r3 y00000

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Person, Contract, Verifier_Contract
 from rest_framework import viewsets
 from .serializers import PersonSerializer, ContractSerializer, Verifier_ContractSerializer
+from django.http import HttpResponse
 # Create your views here.
 
 class PersonViewSet(viewsets.ModelViewSet):
@@ -15,3 +16,10 @@ class ContractViewSet(viewsets.ModelViewSet):
 class VerifierContractViewSet(viewsets.ModelViewSet):
 	queryset = Verifier_Contract.objects.all()
 	serializer_class = Verifier_ContractSerializer
+
+
+def score(request):
+	resulting_scores = Verifier_Contract.objects.all()
+	for x in resulting_scores:
+		print(x)
+	return render(request, "score.html", {})
--- trustcoin/urls.py
+++ trustcoin/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.urls import path
 from rest_framework import routers
 from django.contrib import admin
 from django.http import HttpResponse
@@ -29,6 +30,7 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     # path('', lambda a : HttpResponse("Fuck off")),
     url(r'^', include(router.urls)),
+    path("score/", views.score)
     # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
 ]
