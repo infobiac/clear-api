f41419dab9dc4cb6a31a387749e6cf04f57efe04
Christophe Rimann <cjr2185@columbia.edu>
2018-10-19 12:41:52 -0400

    cors take2

--- trustcoin/settings.py
+++ trustcoin/settings.py
@@ -50,18 +50,24 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'trustcoin.urls'
 
