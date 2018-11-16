48fab12862a77e32c1e2a8b09c331430e09902c8
Christophe Rimann <cjr2185@columbia.edu>
2018-11-15 19:14:25 -0500

    removed references to vyper

--- api/views.py
+++ api/views.py
@@ -8,7 +8,7 @@ import random
 import networkx as nx
 import pickle
 from django.views.decorators.csrf import csrf_exempt
-from vython import *
+# from vython import *
 from random import randint
 
 UUID_not_name = True
@@ -93,57 +93,57 @@ def prscore(request):
 		count += 1
 
 	return HttpResponse(resp)
-class Safe:    
-    value = public(wei_value()) #Value of the item
-    seller = public(address())
-    buyer = public(address())
-    unlocked = public(bool())
-    val = public(uint256())
-    #@constant
-    #def unlocked() -> bool: #Is a refund possible for the seller?
-    #    return (self.balance == self.value*2)
+# class Safe:    
+#     value = public(wei_value()) #Value of the item
+#     seller = public(address())
+#     buyer = public(address())
+#     unlocked = public(bool())
+#     val = public(uint256())
+#     #@constant
+#     #def unlocked() -> bool: #Is a refund possible for the seller?
+#     #    return (self.balance == self.value*2)
 
-    @public
-    @payable
-    def __init__():
-        assert (msg.value % 2) == 0
-        self.value = msg.value / 2  #The seller initializes the contract by
-            #posting a safety deposit of 2*value of the item up for sale.
-        self.seller = msg.sender
-        self.unlocked = True
+#     @public
+#     @payable
+#     def __init__():
+#         assert (msg.value % 2) == 0
+#         self.value = msg.value / 2  #The seller initializes the contract by
+#             #posting a safety deposit of 2*value of the item up for sale.
+#         self.seller = msg.sender
+#         self.unlocked = True
 
-    @public
-    def abort():
-        assert self.unlocked #Is the contract still refundable?
-        assert msg.sender == self.seller #Only the seller can refund
-            # his deposit before any buyer purchases the item.
-        selfdestruct(self.seller) #Refunds the seller and deletes the contract.
+#     @public
+#     def abort():
+#         assert self.unlocked #Is the contract still refundable?
+#         assert msg.sender == self.seller #Only the seller can refund
+#             # his deposit before any buyer purchases the item.
+#         selfdestruct(self.seller) #Refunds the seller and deletes the contract.
 
-    @public
-    @payable
-    def purchase():
-        assert self.unlocked #Is the contract still open (is the item still up for sale)?
-        assert msg.value == (2 * self.value) #Is the deposit the correct value?
-        self.buyer = msg.sender
-        self.unlocked = False
+#     @public
+#     @payable
+#     def purchase():
+#         assert self.unlocked #Is the contract still open (is the item still up for sale)?
+#         assert msg.value == (2 * self.value) #Is the deposit the correct value?
+#         self.buyer = msg.sender
+#         self.unlocked = False
 
-    @public
-    def received():
-        assert not self.unlocked #Is the item already purchased and pending confirmation
-            # from the buyer?
-        assert msg.sender == self.buyer
-        send(self.buyer, self.value) #Return the buyer's deposit (=value) to the buyer.
-        selfdestruct(self.seller) #Return the seller's deposit (=2*value)
-            # and the purchase price (=value) to the seller.
-@csrf_exempt
-def create_new_contract(request):
-	if request.method == "GET":
-		return render(request, "create.html", {})
-	elif request.method == "POST":
-		test = deploy(transpile(Safe))
-		print(test["transactionHash"].hex())
-		return HttpResponse("Contract complete. Transaction ID: {}".format(test["transactionHash"].hex()))
-	return HttpResponse("unsupported method")
+#     @public
+#     def received():
+#         assert not self.unlocked #Is the item already purchased and pending confirmation
+#             # from the buyer?
+#         assert msg.sender == self.buyer
+#         send(self.buyer, self.value) #Return the buyer's deposit (=value) to the buyer.
+#         selfdestruct(self.seller) #Return the seller's deposit (=2*value)
+#             # and the purchase price (=value) to the seller.
+# @csrf_exempt
+# def create_new_contract(request):
+# 	if request.method == "GET":
+# 		return render(request, "create.html", {})
+# 	elif request.method == "POST":
+# 		test = deploy(transpile(Safe))
+# 		print(test["transactionHash"].hex())
+# 		return HttpResponse("Contract complete. Transaction ID: {}".format(test["transactionHash"].hex()))
+# 	return HttpResponse("unsupported method")
 
 def metatest(request):
 	return render(request, "metatest.html", {})
--- trustcoin/settings.py
+++ trustcoin/settings.py
@@ -53,7 +53,8 @@ INSTALLED_APPS = [
     'corsheaders',
     'verifier',
     'verifiee',
-    'seeker'
+    'seeker',
+    'sass_processor',
 ]
 
 
